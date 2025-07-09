f588cb9f82d2220a068259b276d3d435abee00b7
HiHi <hihi@HiHideMacBook-Air.local>
2025-07-09 17:01:42 +0800

    base

--- pages/order-detail/order-detail.js
+++ pages/order-detail/order-detail.js
@@ -1,1 +1,9 @@
-Page({})
+Page({
+  data: {
+    goods: [
+      {id: 1, name: '经典猪肉大白菜饺', count: 2, price: 28, image: '/images/dishes/dumpling1.jpg'},
+      {id: 2, name: '香煎韭菜鸡蛋饺', count: 1, price: 32, image: '/images/dishes/dumpling2.jpg'}
+    ],
+    totalPrice: 88
+  }
+})
